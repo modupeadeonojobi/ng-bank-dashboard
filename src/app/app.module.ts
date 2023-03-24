a920f2b38d10e57e7b5b6a3a584f6f5c49111c00
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2023-03-24 08:40:02 +0100

    add side bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
